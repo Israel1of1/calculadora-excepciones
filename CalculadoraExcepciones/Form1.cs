3773b46ce135aa416647c3b2cc45969998dafd0f
Israel1of1 <israeljimenez2007@icloud.com>
2026-04-18 22:38:20 -0600

    Diseno + seno y coseno con excepciones fEx y oEx

--- CalculadoraExcepciones/Form1.cs
+++ CalculadoraExcepciones/Form1.cs
@@ -16,8 +16,51 @@ namespace CalculadoraExcepciones
         {
             InitializeComponent();
         }
+        private void CalcularSeno()
+        {
+            int numero;
+            double radianes, resultado = 0;
 
-        
+            //para Obtener el numero entero en grados
+            numero = int.Parse(this.txtNumero.Text);
+
+            //convertir a radianes
+            radianes = numero * Math.PI / 180;
+
+            //calculo del seno
+            resultado = Math.Sin(radianes);
+
+            //expresion
+            this.txtExpresion.Text = "sen(" + numero + "°)";
+
+            //resultado
+            this.txtResultado.Text = resultado.ToString();
+
+        }
+
+        private void CalcularCoseno()
+        {
+            int numero;
+            double radianes, resultado = 0;
+
+            //para obtener el entero en grados
+            numero = int.Parse(this.txtNumero.Text);
+
+            //convertir a radianes 
+            radianes = numero * Math.PI / 180;
+
+            //calculo del coseno
+            resultado = Math.Cos(radianes);
+
+            //expresion
+            this.txtExpresion.Text = "cos(" + numero + "°)";
+
+            //Resultado
+            this.txtResultado.Text = resultado.ToString();
+
+        }
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -25,7 +68,28 @@ namespace CalculadoraExcepciones
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                if ((bool)this.radSeno.Checked)
+                {
+                    this.CalcularSeno();
+                }
+                else if ((bool)this.radCoseno.Checked)
+                {
+                    this.CalcularCoseno();
+                }
+            }
+
+            catch (FormatException fEx)
+            {
+                //Si el user escribe letras o deja vacio
+                this.txtResultado.Text = fEx.Message;
+            }
+            catch (OverflowException oEx)
+            {
+                //Error si el num es demasiado grande
+                this.txtResultado.Text = oEx.Message;
+            }
         }
     }
 }
